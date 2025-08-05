54bea48a4983862a4d934ca9ede2cebd88e0a9ab
Otávio Augusto <149170102+Otaavio02@users.noreply.github.com>
2025-08-05 08:51:59 -0300

    Add files via upload

---
+++ firebaseauth/script.js
@@ -0,0 +1,4 @@
+// Obtem os elementos de botão e formulários de login/cadastro
+const singUpButton = doucment.getElementById('signUpButton')
+const signInButton = document.getElementById('signInButton')
+const signInForm = 
