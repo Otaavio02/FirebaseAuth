50afeb23f42b806e3a61fc18065565d1c8d51197
Otávio Augusto <149170102+Otaavio02@users.noreply.github.com>
2025-08-19 08:45:11 -0300

    Delete firebaseauth directory

--- firebaseauth/script.js
+++
@@ -1,4 +0,0 @@
-// Obtem os elementos de botão e formulários de login/cadastro
-const singUpButton = doucment.getElementById('signUpButton')
-const signInButton = document.getElementById('signInButton')
-const signInForm = 
